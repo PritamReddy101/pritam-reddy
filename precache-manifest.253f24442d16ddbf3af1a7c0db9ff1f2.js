7e3e511e69b2a3e0ecb019c05ca1c0928aa45e5d
Your Name <pritam.nandikonda@gmail.com>
2021-11-21 00:35:43 +0530

    Updates

---
+++ precache-manifest.253f24442d16ddbf3af1a7c0db9ff1f2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2c3aae55dd2149aaf4a87c8ea91b56",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5ee004b1e1a8b2c12f08",
+    "url": "/react-portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "e32c7acc8c5f81b1d63b",
+    "url": "/react-portfolio/static/css/main.137f0341.chunk.css"
+  },
+  {
+    "revision": "5ee004b1e1a8b2c12f08",
+    "url": "/react-portfolio/static/js/2.d881c2b4.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/react-portfolio/static/js/2.d881c2b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e32c7acc8c5f81b1d63b",
+    "url": "/react-portfolio/static/js/main.7910a137.chunk.js"
+  },
+  {
+    "revision": "d0f621f0c631a2c92503",
+    "url": "/react-portfolio/static/js/runtime-main.0c4e4ecc.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/react-portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/react-portfolio/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/react-portfolio/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/react-portfolio/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/react-portfolio/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/react-portfolio/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/react-portfolio/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/react-portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/react-portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/react-portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/react-portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/react-portfolio/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/react-portfolio/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/react-portfolio/static/media/myImage.bd80794f.png"
+  }
+]);
